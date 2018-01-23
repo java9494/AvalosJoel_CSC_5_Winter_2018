10165775a2de36cd1a32223d4f51e86c50725c3a
java9494 <joelavalosjr@gmail.com>
2018-01-23 11:27:02 -0800

    Signed-off-by: java9494 <joelavalosjr@gmail.com>

---
+++ Class/Class012318/FutureValue_PowerFunction/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=futurevalue_powerfunction
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/futurevalue_powerfunction
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=futurevaluepowerfunction.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/futurevaluepowerfunction.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=futurevalue_powerfunction
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/futurevalue_powerfunction
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=futurevaluepowerfunction.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/futurevaluepowerfunction.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
